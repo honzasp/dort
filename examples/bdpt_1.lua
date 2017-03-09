7fac06ead384832f4bce387435af914f4adb9634
Jan Špaček <patek.mail@gmail.com>
2017-03-09 18:47:58 +0100

    Prepare the BDPT samples v.1

--- examples/bdpt_1.lua
+++ examples/bdpt_1.lua
@@ -1,115 +1,174 @@
+require "minecraft"
 local _ENV = require "dort/dsl"
-local s = 1e-1
 
-local cornell_box_scene = define_scene(function()
-  local white = matte_material { color = rgb(0.5, 0.5, 0.5) }
-  local green = matte_material { color = rgb(0, 0.5, 0) }
-  local red = matte_material { color = rgb(0.5, 0, 0) }
+function cornell_box_scene(surface_kind, light_kind)
+  local s = 0.01
+  return define_scene(function()
+    local white = matte_material { color = rgb(0.5, 0.5, 0.5) }
+    local green = matte_material { color = rgb(0, 0.5, 0) }
+    local red = matte_material { color = rgb(0.5, 0, 0) }
+    local glossy_white = phong_material { color = rgb(0.5), exponent = 50 }
+    local glossy_red = phong_material { color = rgb(0.5, 0, 0), exponent = 50 }
+    local mirror = mirror_material { color = rgb(0.9) }
+    local glass = glass_material { color = rgb(0.9) }
 
-  local right_box = mirror_material { color = rgb(1, 1, 1) }
-  local left_box = glass_material { color = rgb(1, 1, 1) }
-  right_box = white
-  left_box = white
+    local right_box = white
+    local left_box = white
+    local right_wall = red
+    local left_wall = green
+    local floor = white
 
-  block(function()
-    local m = mesh {
-      points = {
-        point(552.8*s, 0, 0),
-        point(0, 0, 0),
-        point(0, 0, 559.2*s),
-        point(549.6*s, 0, 559.2*s),
+    if surface_kind == "cornell" then
+    elseif surface_kind == "glossy" then
+      right_box = glossy_white
+      left_box = glossy_white
+      right_wall = glossy_red
+    elseif surface_kind == "delta" then
+      right_box = mirror
+      left_box = glass
+      left_wall = mirror
+    end
 
-        point(556*s, 548.8*s, 0),
-        point(556*s, 548.8*s, 559.2*s),
-        point(0, 548.8*s, 559.2*s),
-        point(0, 548.8*s, 0),
-      },
-      vertices = {
-        0, 1, 2,  0, 2, 3,
-        4, 5, 6,  4, 6, 7,
-        6, 5, 3,  6, 3, 2,
-        7, 6, 2,  7, 2, 1,
-        0, 3, 5,  0, 5, 4,
-      },
-    }
+    block(function()
+      local m = mesh {
+        points = {
+          point(552.8*s, 0, 0),
+          point(0, 0, 0),
+          point(0, 0, 559.2*s),
+          point(549.6*s, 0, 559.2*s),
 
-    material(white)
-    add_triangle(m, 0)
-    add_triangle(m, 3)
-    add_triangle(m, 6)
-    add_triangle(m, 9)
-    add_triangle(m, 12)
-    add_triangle(m, 15)
+          point(556*s, 548.8*s, 0),
+          point(556*s, 548.8*s, 559.2*s),
+          point(0, 548.8*s, 559.2*s),
+          point(0, 548.8*s, 0),
+        },
+        vertices = {
+          0, 1, 2,  0, 2, 3,
+          4, 5, 6,  4, 6, 7,
+          6, 5, 3,  6, 3, 2,
+          7, 6, 2,  7, 2, 1,
+          0, 3, 5,  0, 5, 4,
+        },
+      }
 
-    material(green)
-    add_triangle(m, 18)
-    add_triangle(m, 21)
+      material(floor)
+      add_triangle(m, 0)
+      add_triangle(m, 3)
+      add_triangle(m, 6)
+      add_triangle(m, 9)
+      add_triangle(m, 12)
+      add_triangle(m, 15)
 
-    material(red)
-    add_triangle(m, 24)
-    add_triangle(m, 27)
-  end)
+      material(left_wall)
+      add_triangle(m, 18)
+      add_triangle(m, 21)
 
-  block(function()
-    material(left_box)
-    transform(
-        translate(185*s, 82.5*s, 169*s)
-      * rotate_y(-0.29) 
-      * scale(165*s / 2))
-    add_shape(cube())
-  end)
+      material(right_wall)
+      add_triangle(m, 24)
+      add_triangle(m, 27)
+    end)
 
-  block(function()
-    material(right_box)
-    transform(
-        translate(368*s, 165*s, 351*s) 
-      * rotate_y(-1.27) 
-      * scale(165*s / 2, 330*s / 2, 165*s / 2))
-    add_shape(cube())
-  end)
+    block(function()
+      material(left_box)
+      transform(
+          translate(185*s, 82.5*s, 169*s)
+        * rotate_y(-0.29) 
+        * scale(165*s / 2))
+      add_shape(cube())
+    end)
 
-  camera(pinhole_camera {
-    transform = look_at(
-      point(278*s, 273*s, -800*s),
-      point(278*s, 273*s, 0),
-      vector(0, 1, 0)) * scale(1, -1, 1),
-    fov = 0.686,
-  })
+    block(function()
+      material(right_box)
+      transform(
+          translate(368*s, 165*s, 351*s) 
+        * rotate_y(-1.27) 
+        * scale(165*s / 2, 330*s / 2, 165*s / 2))
+      add_shape(cube())
+    end)
 
-  block(function() 
-    transform(translate(0, -1, 0))
-    local m = mesh {
-      points = {
-        point(343*s, 548.8*s, 227*s),
-        point(343*s, 548.8*s, 332*s),
-        point(213*s, 548.8*s, 332*s),
-        point(213*s, 548.8*s, 227*s),
-      },
-      vertices = {
-        2, 1, 0,  3, 2, 0,
-      },
-    }
-    for _, index in ipairs({0, 3}) do
-      add_light(diffuse_light {
-        radiance = rgb(1, 1, 1) * 100,
-        shape = triangle(m, index),
-        num_samples = 8,
+    camera(pinhole_camera {
+      transform = look_at(
+        point(278*s, 273*s, -800*s),
+        point(278*s, 273*s, 0),
+        vector(0, 1, 0)) * scale(1, -1, 1),
+      fov = 0.686,
+    })
+
+    if light_kind == "area" then
+      block(function() 
+        transform(translate(0, -1*s, 0))
+        local m = mesh {
+          points = {
+            point(343*s, 548.8*s, 227*s),
+            point(343*s, 548.8*s, 332*s),
+            point(213*s, 548.8*s, 332*s),
+            point(213*s, 548.8*s, 227*s),
+          },
+          vertices = {
+            2, 1, 0,  3, 2, 0,
+          },
+        }
+        for _, index in ipairs({0, 3}) do
+          add_light(diffuse_light {
+            radiance = rgb(1, 1, 1) * 100,
+            shape = triangle(m, index),
+          })
+        end
+      end)
+    elseif light_kind == "point" then
+      add_light(point_light {
+        point = point(250*s, 400*s, 250*s),
+        intensity = rgb(1) * 15e4 *s*s,
       })
     end
   end)
-  --[[
-  add_light(point_light {
-    point = point(250*s, 300*s, 250*s),
-    intensity = rgb(1) * 3e5 *s*s,
-  })
-  --]]
-end)
+end
+
+function cavern_scene()
+  return define_scene(function()
+    block(function()
+      minecraft.add_world(get_builder(), {
+        map = os.getenv("HOME") .. "/.minecraft/saves/Cavern",
+        box = boxi(vec3i(-30, 80, -30), vec3i(30, 100, 30)),
+      })
+    end)
+
+    local lights = {
+      {point(-4.2, 94.3, -4.8), 150},
+      {point(-12.0, 94.1, -5.5), 300},
+      {point(-6.3, 96.7, 7.1), 150},
+      {point(-1.1, 98.2, 11.9), 300},
+      {point(-5.2, 92.9, -2.2), 150},
+    }
+
+    for _, light_pair in ipairs(lights) do
+      --[[
+      add_light(point_light {
+        point = light_pair[1],
+        intensity = rgb(light_pair[2]),
+      })
+      --]]
+      add_light(diffuse_light {
+        shape = sphere { radius = 0.3 },
+        radiance = rgb(light_pair[2] * 3),
+        transform = translate(light_pair[1] - point(0,0,0)),
+      })
+    end
+
+    camera(pinhole_camera {
+      transform = look_at(
+        point(3.5, 100.4, -0.1),
+        point(10.7, 92.3, -10.4),
+        vector(0, 1, 0)) * scale(-1, -1, 1),
+      fov = pi / 3,
+    })
+  end)
+end
 
 res = 512
 common_opts = {
   x_res = res, y_res = res,
   hdr = true,
-  max_depth = 5,
   sampler = stratified_sampler {
     samples_per_x = 2,
     samples_per_y = 2,
@@ -118,45 +177,64 @@ common_opts = {
     radius = 1.5,
   }
 }
-opts = {
-  direct = {
+algos = {
+  --[[
+  {"dot", {
+    renderer = "dot",
+  }},
+  {"direct", {
+    max_depth = 5,
     renderer = "direct",
-    iterations = 4,
-  },
-  pt = {
+    iterations = 8,
+  }},
+  {"pt", {
+    max_depth = 5,
     renderer = "pt",
     iterations = 5,
-  },
-  bdpt = {
+  }},
+  {"bdpt", {
+    max_depth = 5,
     renderer = "bdpt",
     iterations = 5,
-  },
-  ref = {
+  }},
+  --]]
+  {"ref", {
+    max_depth = 5,
     renderer = "sppm",
-    iterations = 20,
-    initial_radius = 100*s,
+    iterations = 2000,
+    initial_radius = 0.5,
     light_paths = res*res,
-  },
+  }},
+  --]]
 }
 scenes = {
-  cornell_box = cornell_box_scene,
+  --{"cornell_box", cornell_box_scene("cornell", "area")},
+  {"glossy_box", cornell_box_scene("glossy", "area")},
+  --{"delta_box", cornell_box_scene("delta", "area")},
+  --{"cornellp_box", cornell_box_scene("cornell", "point")},
+  {"glossyp_box", cornell_box_scene("glossy", "point")},
+  --{"deltap_box", cornell_box_scene("delta", "point")},
+  {"cavern", cavern_scene()},
 }
-out_dir = "/home/patek/Downloads/bdpt_1"
+out_dir = "."
 
-for algo_name, algo_opts in pairs(opts) do
-  for scene_name, scene in pairs(scenes) do
+for algo_i = 1, #algos do
+  local algo_name = algos[algo_i][1]
+  for scene_i = 1, #scenes do
+    local scene_name = scenes[scene_i][1]
+
     opts = {}
     for key, value in pairs(common_opts) do
       opts[key] = value
     end
-    for key, value in pairs(algo_opts) do
+    for key, value in pairs(algos[algo_i][2]) do
       opts[key] = value
     end
 
     image_name = scene_name.."_"..algo_name
     print(image_name)
 
-    hdr_image = render(scene, opts)
+    hdr_image = render(scenes[scene_i][2], opts)
     write_rgbe_image(out_dir.."/"..image_name..".hdr", hdr_image)
     srgb_image = tonemap_srgb(hdr_image)
     write_png_image(out_dir.."/"..image_name..".png", srgb_image)
